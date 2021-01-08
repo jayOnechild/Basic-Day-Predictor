76de039e35f8ee64974863ea1daa3ce7068640a5
jayOnechild <jacksonmwanaumo02@gmail.com>
2021-01-09 01:20:08 +0200

    added the year

--- home.js
+++ home.js
@@ -5,15 +5,17 @@ function today(){
 
     //initialising the day  (as a number e.g Monday=1)
     var actualDate = d.getDay();
+
+    var actualYear = "<h1>"+d.getFullYear()+"</h1>";
     
     //Initialising all days of the week
-    var sun = '<h1>Its a Blessed Sunday</h1>';
-    var mon = 'Its a Productive Monday';
-    var tue = 'Its an Exciting Tuesday';
-    var wed = 'Its a Great Wednesday';
-    var thu = 'Its an Exciting Thursday';
-    var fri = 'Its a Lit! Friday';
-    var sat = '<h1>Its an Amazing Saturday</h1>';
+    var sun = '<h1>Its a Blessed Sunday</h1>'+ actualYear;
+    var mon = 'Its a Productive Monday'+ actualYear;
+    var tue = 'Its an Exciting Tuesday'+ actualYear;
+    var wed = 'Its a Great Wednesday'+ actualYear;
+    var thu = 'Its an Exciting Thursday'+ actualYear;
+    var fri = 'Its a Lit! Friday'+ actualYear;
+    var sat = '<h1>Its an Amazing Saturday of </h1>'+ actualYear;
  
     //just for fun addition
     let def = "It's an awesome day.";
